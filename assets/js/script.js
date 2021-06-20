0c841f1c81b1746029fcfa09ea7c7a389c559d6a
Paula Meliqueo <pameliqueo@uc.cl>
2021-06-20 14:51:46 -0400

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+$(function(){
+    $("a").click(function(event){
+     if (this.hash !== ""){
+       event.preventDefault();
+       
+       var gato = this.hash;
+
+       $("html, body").animate({
+           scrollTop: $(gato).offset().top
+       }, 800, function(){
+           window.location.hash = gato;
+       });   
+     }
+});
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   $(function(){
     $("#titulo1").click(function(){
       $("#texto1").hide(500);
